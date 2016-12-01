6a56933b31f7b110856803abdde34a04ab3566a1
Tibs12380 <dmisdizzy@gmail.com>
2016-12-01 11:44:22 +0000

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,50 @@
+//Person.java
+/*
+ *
+ */
+ 
+ public class Person{
+ 	private String name;
+ 	private String hairColor;
+ 	private int age;
+ 	
+ 	public Person(){
+ 		this.name = "Unknown";
+ 		this.hairColor = "Grey";
+ 		this.age = "33";
+ 	}
+ 	public Person(String name, String hairColor, int age){
+ 		this.name=name;
+ 		this.hairColor=hairColor;
+ 		this.age=age;
+ 	}
+ 	
+ 	
+ 	public void setName(name){
+ 		this.name=name;
+ 	}
+ 
+ 	public void setHairColor(hairColor){
+ 		this.hairColor=hairColor;
+ 	}
+ 	
+ 	public void setAge(age){
+ 		this.age=age;
+ 	}
+ 	 	
+ 	/** getName
+ 	 * returns the name  of the character.
+ 	 *@return The name of the character the method was called on
+ 	 */
+ 	public String getName(){
+ 		return name;
+ 	}
+ 	
+ 	public String gethairColor(){
+ 		return hairColor;
+ 	}
+ 	
+ 	public int getAge(){
+ 		return age;
+ 	}
+ }
---
+++ Player.java
@@ -0,0 +1,62 @@
+//Player.java
+/*
+ *
+ */
+ 
+public Class Player extends Person{
+	private String characterClass;
+	private String race;
+	private String gender;
+	
+	/** Player
+	 *Creates a new player based on default values. called when the user enters no values there there charcter
+	 */
+	public Player(){
+		this.characterClass = "Blade Master";
+		this.race ="Human";
+		this.gender = "Male"; 
+	}
+	
+	
+	/** Player
+	 *Creates a new player using the vaules enterd by the user
+	 *@param characterClass The class the user has shosen for there character
+	 *@param race The species the character is, chosen by the user
+	 *@param gender whether the character is male or female
+	 *@param name the name of the character
+	 *@param hairColor the color of the characters hair
+	 *@param age the age of the character.
+	 */
+	public Player(String characterClass, String race, String gender, String name, String hairColor, int age){
+		this.characterClass = characterClass;
+		this.race = race;
+		this.gender =gender;
+		setName(name);
+		setHairColor(hairColor);
+		setAge(age);
+	}
+	
+	public void setCharacterClass(String characterClass){
+		this.characterClass=characterClass;
+	}	
+	
+	public void setRace(String race){
+		this.race=race;
+	}
+	
+	public void setGender(String gender){
+		this.gender=gender; 
+	}
+	
+	public String getCharacterClass(){
+		return characterClass;
+	} 
+	
+	public String getRace(){
+		return race;
+	}	
+	
+	public String getGender(){
+		return gender;
+	}	
+}
