26d48a69720086ee905548ad00dbeaee1e402ab6
Tibs12380 <dmisdizzy@gmail.com>
2016-12-07 15:07:43 +0000

    Add files via upload

--- Gui.java
+++ Gui.java
@@ -2,12 +2,31 @@
 /*
  *
  */
- 
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class Gui extends JFrame implements ActionListener{
+
+    String name;
+    String hairColor;
+    String race;
+    String characterClass;
+    String gender;
+    int age;
+
+    private JTextArea textPane = new JTextArea("Would you like to create a new character?");
+    JButton defaultCharacter = new JButton("Use Default Character");
+    JButton customCharacter = new JButton("Create Custom Character");
+    JButton human = new JButton("Human");
+    JButton dwarf = new JButton("Dwarf");
+    JButton elf = new JButton("Elf");
+    JButton bladeMaster = new JButton("Blade Master");
+    JButton tank = new JButton("Tank");
+    JButton trickster = new JButton("Trickster");
+    Container pane = getContentPane();
+    Player p1 = new Player();
 
     //driver
     public static void main(String args[]){
@@ -21,23 +40,59 @@ public class Gui extends JFrame implements ActionListener{
         setTitle     ( "The Adventure up North" );
         setSize      ( 400,200 );
         setLocation  ( 100,100 );
-        Container pane = getContentPane();
+
+        FlowLayout layout = new FlowLayout();
+        pane.setLayout(layout);
+        textPane.setBackground(Color.GRAY);
+        textPane.setForeground((Color.WHITE));
+        pane.add(textPane);
+        defaultCharacter.addActionListener(this);
+        customCharacter.addActionListener(this);
+        pane.add(defaultCharacter);
+        pane.add(customCharacter);
         pane.setBackground(Color.gray);
+
+
+
         //register 'Exit upon closing' as a default close operation
-        setDefaultCloseOperation( EXIT_ON_CLOSE );
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+      //  JOptionPane.showMessageDialog(null,"Do you want to create you on character?");
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == customCharacter) {
+            name = JOptionPane.showInputDialog(null,"What is your name");
+            hairColor = JOptionPane.showInputDialog(null,"What is your haircolor");
+            age = Integer.parseInt(JOptionPane.showInputDialog(null,"What is your age?"));
+            pane.remove(customCharacter);
+            pane.remove(defaultCharacter);
+            textPane.setText("Please chose your race");
+            human.addActionListener(this);
+            pane.add(human);
+            dwarf.addActionListener(this);
+            pane.add(dwarf);
+            elf.addActionListener(this);
+            pane.add(elf);
+            pane.repaint();
+        }
+         else if(e.getSource() == human){
+            race = "Human";
+            textPane.append("\n\n\nWhat is your class?");
+            bladeMaster.addActionListener(this);
+            pane.add(bladeMaster);
+            tank.addActionListener(this);
+            pane.add(tank);
+            trickster.addActionListener(this);
+            pane.add(trickster);
+        }
+
+        else if (e.getSource() == defaultCharacter){
+
+
+        }
+
 
     }
-
-    //Make buttons
-    public class buttons {
-        JButton Start = new JButton("Open to the first page");
-        JButton defaultCharacter = new JButton("Use default character");
-        JButton Customised = new JButton("Create your own character");
-    }
-
 }
 
