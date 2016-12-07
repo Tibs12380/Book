165359b74a22896f469c815f83556639fa8ed018
Tibs12380 <dmisdizzy@gmail.com>
2016-12-07 12:28:12 +0000

    Add files via upload

--- Gui.java
+++ Gui.java
@@ -8,31 +8,36 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Gui extends JFrame implements ActionListener{
-	
-	//driver
-	public static void main(Sring args[]){
-		Gui frame = new Gui();
-		frame.setVisible(true);
-	}
-	
+
+    //driver
+    public static void main(String args[]){
+        Gui frame = new Gui();
+        frame.setVisible(true);
+    }
+
 	 // constructor
-    public Gui( ) {
+    public Gui() {
         //set the frame default properties
-        setTitle     ( "The Adventures of You" );
+        setTitle     ( "The Adventure up North" );
         setSize      ( 400,200 );
         setLocation  ( 100,100 );
         Container pane = getContentPane();
-        //pane.setBackground(Color.blue);
-        pane.setBackground(new Color(0,20,40));
+        pane.setBackground(Color.gray);
         //register 'Exit upon closing' as a default close operation
         setDefaultCloseOperation( EXIT_ON_CLOSE );
-    }  
-    
-    //Make buttons
-    
-    Start = new JButton("Open to the first page");
-    defualt = new JButton("Use default character");
-    Customised = new JButton("Create your own character");
+    }
 
-    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    //Make buttons
+    public class buttons {
+        JButton Start = new JButton("Open to the first page");
+        JButton defaultCharacter = new JButton("Use default character");
+        JButton Customised = new JButton("Create your own character");
+    }
+
 }
+
